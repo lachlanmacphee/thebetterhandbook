9b88b1c58c03e600a926d77ecc06c12b77ac5cbf
Lachlan MacPhee <github@lachlanmacphee.com>
2025-11-01 00:16:12 +1100

    fix: more detailed auth screen

--- app/routes/auth.login.tsx
+++ app/routes/auth.login.tsx
@@ -48,41 +48,38 @@ export default function Route() {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const email = formData.get("email") as string;
-    const pattern = /^[a-zA-Z0-9._%+-]+@student\.monash\.edu$/;
 
-    if (!pattern.test(email)) {
-      setEmailError(
-        "Please use your Monash student email (abcd0001@student.monash.edu)"
-      );
+    if (
+      email.endsWith("@student.monash.edu") ||
+      email.endsWith("@student.unimelb.edu.au")
+    ) {
+      setEmailError("");
+      fetcher.submit(formData, { method: "POST" });
       return;
     }
 
-    setEmailError("");
-    fetcher.submit(formData, { method: "POST" });
+    setEmailError("Please use your student email");
   };
 
   return (
     <>
       <h1 className="text-2xl font-semibold mb-2">Sign In</h1>
-      <fetcher.Form
-        method="POST"
-        className="space-y-6"
-        onSubmit={handleSubmit}
-      >
+      <fetcher.Form method="POST" className="space-y-6" onSubmit={handleSubmit}>
         <label>
           Email Address
           <input
             type="email"
             name="email"
-            placeholder="abcd0001@student.monash.edu"
             required
             autoComplete="email"
             aria-label="Email"
             aria-describedby="email-helper"
+            aria-invalid={emailError != "" || errors}
             disabled={isSubmitting}
           />
           <small id="email-helper">
-            Please use your Monash student email.
+            Your email must end in either @student.monash.edu or
+            @student.unimelb.edu.au
           </small>
         </label>
         <div className="grid">
@@ -94,11 +91,8 @@ export default function Route() {
           >
             Go back
           </button>
-          <button
-            type="submit"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? "Sending..." : "Continue"}
+          <button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Continue"}
           </button>
         </div>
       </fetcher.Form>
